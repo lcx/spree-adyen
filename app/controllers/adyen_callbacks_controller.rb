07f781736e493375382ed4ba84494e6a47c2d655
Maxime Robert-Schreyers <maxime.robert.s@gmail.com>
2010-12-22 12:19:42 +0100

    callbacks work, authorization works but feels weak

---
+++ app/controllers/adyen_callbacks_controller.rb
@@ -0,0 +1,39 @@
+class AdyenCallbacksController < Spree::BaseController
+  include ActiveMerchant::Billing::Integrations
+#  before_filter :adyen_auth
+  protect_from_forgery :except => :index
+
+  # this action is called by Adyen
+  # and _not_ by the end user
+  def index
+    notify = ActiveMerchant::Billing::Integrations::Adyen::Notification.new(request.query_string)
+
+    @order = Order.find_by_number(notify.item_id)
+    begin
+      if notify.complete?
+        @payment = @order.payment
+        @payment.source = BillingIntegration::Adyen.current
+        @payment.response_code = notify.event_code
+        @payment.started_processing!
+#        @order.psp_reference = notify.transaction_id
+        @order.next!
+      else
+        logger.error("Couldn't verify payment! Order id: #{@order.id}")
+      end
+    rescue => e
+      raise
+    ensure
+      @order.save!
+    end
+    
+    render :text => "[accepted]"
+  end
+
+  private
+  def adyen_auth
+    authenticate_or_request_with_http_basic do |user, pass|
+      user == ADYEN_NOTIFY_USER and pass == ADYEN_NOTIFY_PASS
+    end
+  end
+
+end
